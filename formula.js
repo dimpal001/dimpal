25f00d6447fc6c2336464efa48d26df748376a48
dimpal001 <dimpaldas518@gmail.com>
2023-02-28 11:49:46 +0530

    adding some animation

--- formula.js
+++ formula.js
@@ -1,17 +1,15 @@
 $(function(){
     $("#alerts1").click(function(){
-        $("#sgpa").fadeOut(500);
-        $("#sgpa").fadeIn(900);
+        $("#prependHere").slideUp(600).slideDown(600);
         setTimeout(function(){
             $("#alerts1").addClass("d-none");
-        $("#sgpa").addClass("px-md-3");
-        $("#alert-text").text("This website is temporarily down for maintenance. We will be back online shortly.")
-        $("#alerts2").removeClass("d-none");
+            $("#sgpa").addClass("px-md-3");
+            $("#alert-text").text("This website is temporarily down for maintenance. We will be back online shortly.")
+            $("#alerts2").removeClass("d-none");
         },600)
     })
     $("#alerts2").click(function(){
-        $("#sgpa").fadeOut(500);
-        $("#sgpa").fadeIn(900);
+        $("#prependHere").slideUp(600).slideDown(600);
         setTimeout(function(){
         $("#alerts2").addClass("d-none");
         $("#alert-text").text("SGPA Calculator")
@@ -19,15 +17,22 @@ $(function(){
         },600)
     })
     $("#btn").click(function(){
-        $("#main").fadeOut(500);
-        $("#main").fadeIn(900);
+        $("#main").slideUp(600).slideDown(600);
         $("#btn").fadeOut(500);
         setTimeout(function(){
+            $("#btn2").removeClass("d-none");
             $("#myProfile").html("Welcome<br><h6>You are already in this website.</h6>")
         },600)
     })
+    $("#btn2").click(function(){
+        $("#main").slideUp(600).slideDown(600);
+        setTimeout(function(){
+            $("#btn").fadeIn();
+            $("#btn2").addClass("d-none");
+            $("#myProfile").text("My Profile")
+        },600)
+    })
     $("#imgs").click(function(){
-        $("#imgs").fadeOut(500);
-        $("#imgs").fadeIn(500);
+        $("#imgs").slideUp(600).slideDown(600);
     })
 });
